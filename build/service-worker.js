dd62169bf464d771788aea1e930484836a5eb933
Frankie Mercado <oeluer@gmail.com>
2022-02-21 18:40:50 -0800

    add build to git

---
+++ build/service-worker.js
@@ -0,0 +1,104 @@
+const timestamp = 1645497001514;
+const build = [
+  "/_app/start-b429563c.js",
+  "/_app/pages/__layout.svelte-46a7f637.js",
+  "/_app/assets/pages/__layout.svelte-f178ade1.css",
+  "/_app/pages/__error.svelte-793cd5ee.js",
+  "/_app/assets/pages/__error.svelte-b9ae05d2.css",
+  "/_app/pages/index.svelte-23b35f89.js",
+  "/_app/assets/pages/index.svelte-bf7e6faa.css",
+  "/_app/pages/project/index.svelte-1cfb1f50.js",
+  "/_app/assets/pages/project/index.svelte-7ef586c9.css",
+  "/_app/pages/project/brainly-scraper-ts/index.svx-5f8b9f29.js",
+  "/_app/assets/prism-night-owl-17eca136.css",
+  "/_app/pages/project/old-personal-site/index.svx-3fa7ba94.js",
+  "/_app/pages/project/ci-inventory-app/index.svx-0377c395.js",
+  "/_app/pages/project/java-cashier-app/index.svx-1d860154.js",
+  "/_app/pages/project/nyaa-si-scraper/index.svx-3fdb8fdb.js",
+  "/_app/pages/project/online-library/index.svx-34474b6d.js",
+  "/_app/pages/project/covid-info-v2/index.svx-01ee64eb.js",
+  "/_app/pages/project/svelteception/index.svx-193df55f.js",
+  "/_app/pages/project/school-stuff/index.svx-ca329d57.js",
+  "/_app/pages/project/covid-info/index.svx-ca234f7d.js",
+  "/_app/assets/Update.svelte_svelte_type_style_lang-28d817af.css",
+  "/_app/pages/project/kana-board/index.svx-a2902480.js",
+  "/_app/pages/project/umaru-chat/index.svx-fe20441f.js",
+  "/_app/pages/project/graphene/index.svx-e72626ae.js",
+  "/_app/pages/project/kanaizu/index.svx-8d10fe82.js",
+  "/_app/pages/project/gh-job/index.svx-133ea35e.js",
+  "/_app/pages/project/prawf/index.svx-f5cc23a0.js",
+  "/_app/pages/project/skaga/index.svx-0a8f1d57.js",
+  "/_app/pages/about.svx-1f0899d8.js",
+  "/_app/assets/pages/about.svx-498c167b.css",
+  "/_app/pages/post/index.svelte-da206195.js",
+  "/_app/assets/pages/post/index.svelte-b38320cb.css",
+  "/_app/pages/post/combining-adonis-and-svelte-using-inertia/index.svx-6c608fcd.js",
+  "/_app/pages/post/prettify-screenshot-using-imagemagick/index.svx-03f5ddf8.js",
+  "/_app/pages/post/comments-widget-using-utterance/index.svx-faa852b8.js",
+  "/_app/pages/post/i-rebuild-my-site-using-sapper/index.svx-96524d45.js",
+  "/_app/pages/post/japanese-input-method-on-linux/index.svx-d49279a7.js",
+  "/_app/pages/post/improving-nvim-startup-time/index.svx-ada5deff.js",
+  "/_app/pages/post/my-experience-with-svelte/index.svx-706c52a9.js",
+  "/_app/pages/post/how-i-memorise-heijitsu/index.svx-7815a617.js",
+  "/_app/pages/post/notetaking-with-notion/index.svx-10ea101c.js",
+  "/_app/pages/post/neovim-lua-statusline/index.svx-f7d1aa38.js",
+  "/_app/pages/post/chrome-custom-newtab/index.svx-0c1e26f7.js",
+  "/_app/pages/post/my-spotify-tui-setup/index.svx-5e0bc17f.js",
+  "/_app/pages/post/trying-out-sveltekit/index.svx-491927c4.js",
+  "/_app/pages/post/making-of-my-site-2/index.svx-bc7a120f.js",
+  "/_app/pages/post/making-of-my-site-3/index.svx-2e9914b0.js",
+  "/_app/pages/post/rest-client-for-vim/index.svx-b37c78e9.js",
+  "/_app/pages/post/a-year-of-japanese/index.svx-81dba07d.js",
+  "/_app/pages/post/from-sapper-to-kit/index.svx-c3a65bb8.js",
+  "/_app/pages/post/es6-array-methods/index.svx-dedf3a6b.js",
+  "/_app/pages/post/making-of-my-site/index.svx-01cc8a6b.js",
+  "/_app/pages/post/my-nvim-lsp-setup/index.svx-679a943a.js",
+  "/_app/pages/post/my-opinion-on-nix/index.svx-8a6e41d7.js",
+  "/_app/pages/post/my-suckless-setup/index.svx-b8abc8fe.js",
+  "/_app/pages/post/why-i-use-linux/index.svx-2680d9c4.js",
+  "/_app/pages/post/github-actions/index.svx-d9d42ed5.js",
+  "/_app/pages/post/vim-statusline/index.svx-dbcf355a.js",
+  "/_app/pages/post/site-redesign/index.svx-760bbe77.js",
+  "/_app/pages/post/thinkpad-x220/index.svx-2e254029.js",
+  "/_app/pages/post/defx-nvim/index.svx-d4c8f609.js",
+  "/_app/pages/post/2bwm/index.svx-07582bae.js",
+  "/_app/chunks/vendor-a3554607.js",
+  "/_app/chunks/theme-4e4001f1.js",
+  "/_app/chunks/stores-5109c37e.js",
+  "/_app/chunks/Progress-912bc64e.js",
+  "/_app/assets/Progress-a5e343ff.css",
+  "/_app/chunks/PostCard-e3afe0ff.js",
+  "/_app/assets/PostCard-c7ffcd66.css",
+  "/_app/chunks/ProjectCard-c9743bf2.js",
+  "/_app/assets/ProjectCard-88a7f31f.css",
+  "/_app/chunks/github-a76e40b1.js",
+  "/_app/chunks/project-bf85e9b5.js",
+  "/_app/assets/project-b483244c.css",
+  "/_app/chunks/Update-e4617bdb.js",
+  "/_app/chunks/post-1875198a.js",
+  "/_app/assets/post-789c7bc3.css"
+];
+const CACHE_NAME = `cache-${timestamp}`;
+const ASSETS = build.filter((file) => !/.*\.(png|webp|jpg|)/.test(file));
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(async (keys) => {
+    for (const key of keys) {
+      if (!key.includes(timestamp.toString()))
+        caches.delete(key);
+    }
+  }));
+});
+self.addEventListener("fetch", async (event) => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse)
+      return cachedResponse;
+    const networkResponse = await fetch(event.request);
+    event.waitUntil(cache.put(event.request, networkResponse.clone()));
+    return networkResponse;
+  })());
+});
