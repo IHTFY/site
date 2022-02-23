0b20dbb13e1e9e31fcd6d702742001094f0885b2
Frankie Mercado <oeluer@gmail.com>
2022-02-22 20:52:42 -0800

    fix image links

---
+++ build/service-worker.js
@@ -0,0 +1,135 @@
+const timestamp = 1645591947391;
+const build = [
+  "/_app/start-25961ec1.js",
+  "/_app/pages/__layout.svelte-432b0521.js",
+  "/_app/assets/pages/__layout.svelte-f178ade1.css",
+  "/_app/pages/__error.svelte-623ac9a9.js",
+  "/_app/assets/pages/__error.svelte-b9ae05d2.css",
+  "/_app/pages/index.svelte-02f948b1.js",
+  "/_app/assets/pages/index.svelte-bf7e6faa.css",
+  "/_app/pages/project/index.svelte-2ae2f56a.js",
+  "/_app/assets/pages/project/index.svelte-7ef586c9.css",
+  "/_app/pages/project/brainly-scraper-ts/index.svx-a066a845.js",
+  "/_app/assets/prism-night-owl-17eca136.css",
+  "/_app/pages/project/old-personal-site/index.svx-d61e6a3e.js",
+  "/_app/pages/project/ci-inventory-app/index.svx-986ec6d6.js",
+  "/_app/pages/project/java-cashier-app/index.svx-da5adc80.js",
+  "/_app/pages/project/nyaa-si-scraper/index.svx-c07600f5.js",
+  "/_app/pages/project/online-library/index.svx-9efa7b6e.js",
+  "/_app/pages/project/covid-info-v2/index.svx-4e00e4e2.js",
+  "/_app/pages/project/svelteception/index.svx-00d5664b.js",
+  "/_app/pages/project/school-stuff/index.svx-75123120.js",
+  "/_app/pages/project/covid-info/index.svx-97ec075a.js",
+  "/_app/assets/pages/project/covid-info/index.svx-f757a2f6.css",
+  "/_app/pages/project/kana-board/index.svx-bd78f728.js",
+  "/_app/pages/project/umaru-chat/index.svx-18ab1d6d.js",
+  "/_app/pages/project/graphene/index.svx-c123dd24.js",
+  "/_app/pages/project/kanaizu/index.svx-7ac5fd93.js",
+  "/_app/pages/project/gh-job/index.svx-b3435a99.js",
+  "/_app/pages/project/prawf/index.svx-4574d491.js",
+  "/_app/pages/project/skaga/index.svx-1cf8ecf4.js",
+  "/_app/pages/about.svx-8eafb254.js",
+  "/_app/assets/pages/about.svx-498c167b.css",
+  "/_app/pages/post/index.svelte-5f32c529.js",
+  "/_app/assets/pages/post/index.svelte-b38320cb.css",
+  "/_app/pages/post/youtube-speeds-faster-than-2x/index.svx-ee37867c.js",
+  "/_app/pages/post/milk-tea-doppelgangers/index.svx-be9c4fbd.js",
+  "/_app/pages/post/january-retrospective/index.svx-65e79493.js",
+  "/_app/pages/post/calendar-progress/index.svx-9d09249c.js",
+  "/_app/pages/post/drunken-crawfish/index.svx-d8a94e8d.js",
+  "/_app/pages/post/soundcloud-likes/index.svx-353361b8.js",
+  "/_app/pages/post/panorama-scroll/index.svx-15205adf.js",
+  "/_app/pages/post/watching-videos/index.svx-fb4db4dc.js",
+  "/_app/pages/post/happy-new-year/index.svx-0bc68b1b.js",
+  "/_app/pages/post/sleep-schedule/index.svx-5a203d08.js",
+  "/_app/pages/post/chess-engine/index.svx-5bc26ded.js",
+  "/_app/pages/post/pseudorandom/index.svx-3278e48c.js",
+  "/_app/pages/post/winter-drone/index.svx-3fc89c83.js",
+  "/_app/pages/post/albinogator/index.svx-8759b6ad.js",
+  "/_app/pages/post/compression/index.svx-e2180db6.js",
+  "/_app/pages/post/lazy-sunday/index.svx-decb669f.js",
+  "/_app/pages/post/livestreams/index.svx-37a7aebc.js",
+  "/_app/pages/post/days-go-by/index.svx-4381e5a1.js",
+  "/_app/pages/post/early-bird/index.svx-0e51c7bb.js",
+  "/_app/pages/post/oil-change/index.svx-b936b8ed.js",
+  "/_app/pages/post/standalone/index.svx-b95cf0e6.js",
+  "/_app/pages/post/hotel-acs/index.svx-a03ad4e3.js",
+  "/_app/pages/post/hotel-tvs/index.svx-97ee30e7.js",
+  "/_app/pages/post/sveltekit/index.svx-bee3fe69.js",
+  "/_app/pages/post/dash-cam/index.svx-32f8a7cb.js",
+  "/_app/pages/post/headache/index.svx-f078256b.js",
+  "/_app/pages/post/shortner/index.svx-710f2c20.js",
+  "/_app/pages/post/simplify/index.svx-d736bc06.js",
+  "/_app/pages/post/taco-dip/index.svx-20e113cd.js",
+  "/_app/pages/post/westward/index.svx-d8da36a6.js",
+  "/_app/pages/post/ana-mke/index.svx-9a735ea1.js",
+  "/_app/pages/post/ana-raw/index.svx-c266b4e6.js",
+  "/_app/pages/post/anodize/index.svx-d0bccaab.js",
+  "/_app/pages/post/asphalt/index.svx-25648b56.js",
+  "/_app/pages/post/fractal/index.svx-28d20baa.js",
+  "/_app/pages/post/hibachi/index.svx-3c6fe225.js",
+  "/_app/pages/post/offline/index.svx-3e3300cf.js",
+  "/_app/pages/post/theming/index.svx-8ced0b21.js",
+  "/_app/pages/post/workout/index.svx-5f8fb107.js",
+  "/_app/pages/post/cheese/index.svx-cdf46d0d.js",
+  "/_app/pages/post/forgot/index.svx-ef53de6a.js",
+  "/_app/pages/post/globes/index.svx-bf72edc0.js",
+  "/_app/pages/post/knight/index.svx-f9a869b5.js",
+  "/_app/pages/post/mmfunk/index.svx-a4735775.js",
+  "/_app/pages/post/plates/index.svx-06dae902.js",
+  "/_app/pages/post/reredo/index.svx-a016c810.js",
+  "/_app/pages/post/blank/index.svx-20896ec7.js",
+  "/_app/pages/post/games/index.svx-d27ccc91.js",
+  "/_app/pages/post/imslp/index.svx-7c75684b.js",
+  "/_app/pages/post/lingo/index.svx-1de781a4.js",
+  "/_app/pages/post/point/index.svx-cd31e99c.js",
+  "/_app/pages/post/prime/index.svx-28716b17.js",
+  "/_app/pages/post/grey/index.svx-0b984443.js",
+  "/_app/pages/post/late/index.svx-dd7cf5f3.js",
+  "/_app/pages/post/logo/index.svx-3e163f5f.js",
+  "/_app/pages/post/redo/index.svx-0c767bb4.js",
+  "/_app/pages/post/tabs/index.svx-3a6a9c28.js",
+  "/_app/pages/post/wire/index.svx-5f2c912f.js",
+  "/_app/pages/post/444/index.svx-00aa6240.js",
+  "/_app/pages/post/cat/index.svx-b435d924.js",
+  "/_app/pages/post/hdd/index.svx-57e0e7cb.js",
+  "/_app/pages/post/pwa/index.svx-865bab60.js",
+  "/_app/chunks/vendor-a3554607.js",
+  "/_app/chunks/theme-4e4001f1.js",
+  "/_app/chunks/stores-5109c37e.js",
+  "/_app/chunks/Progress-da00dd34.js",
+  "/_app/assets/Progress-a5e343ff.css",
+  "/_app/chunks/PostCard-78d6bb96.js",
+  "/_app/assets/PostCard-c7ffcd66.css",
+  "/_app/chunks/ProjectCard-60eef65c.js",
+  "/_app/assets/ProjectCard-88a7f31f.css",
+  "/_app/chunks/github-a76e40b1.js",
+  "/_app/chunks/project-9e8bca68.js",
+  "/_app/assets/project-b483244c.css",
+  "/_app/chunks/post-d557e900.js",
+  "/_app/assets/post-789c7bc3.css"
+];
+const CACHE_NAME = `cache-${timestamp}`;
+const ASSETS = build.filter((file) => !/.*\.(png|webp|jpg|)/.test(file));
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(async (keys) => {
+    for (const key of keys) {
+      if (!key.includes(timestamp.toString()))
+        caches.delete(key);
+    }
+  }));
+});
+self.addEventListener("fetch", async (event) => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse)
+      return cachedResponse;
+    const networkResponse = await fetch(event.request);
+    event.waitUntil(cache.put(event.request, networkResponse.clone()));
+    return networkResponse;
+  })());
+});
